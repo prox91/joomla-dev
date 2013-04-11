7dc54053a6779d3b19e9f36e77febeea9f1904f4
nha.redweb <nha@redweb.dk>
2013-04-11 12:59:47 +0700

    Commit redtwitter helper

---
+++ components/com_redtwitter/site/libs/twitteroauth/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '8ErgG8KfoEStRltpo76fw');
+define('CONSUMER_SECRET', 'ztACDEIm1oYNGpuDR4JXJZMBrvDia5TIhV93nFJmFIo');
+define('OAUTH_TOKEN', '631697128-8P0RyekbyWDPeOrODTgNNP9ZSdalzaqmJqPtwMij');
+define('OAUTH_TOKEN_SECRET', 'VGrNtcVFXcVq72qs26z3GDI1ClcVijmSotFwZtucSbg');
+define('OAUTH_CALLBACK', 'https://api.twitter.com/oauth/request_token');
