a89a7d45ed814edc50ae739ad7f465bc6594096a
nha.redweb <nha@redweb.dk>
2013-04-11 14:05:52 +0700

    Update source code.

--- components/com_redtwitter/admin/controllers/oauth_infos.php
+++ components/com_redtwitter/admin/controllers/oauth_infos.php
@@ -18,10 +18,46 @@ jimport('joomla.application.component.controller');
 class RedtwitterControllerOauth_Infos extends JControllerLegacy
 {
 	/**
-	 * @param array $default
+	 * Proxy for getModel.
+	 * @since    1.6
 	 */
-	public function __construct($default = array())
+	public function getModel($name = 'oauth_info', $prefix = 'RedtwitterModel')
 	{
-		parent::__construct($default);
+		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+
+		return $model;
+	}
+
+	/**
+	 * Method to save the submitted ordering values for records via AJAX.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.0
+	 */
+	public function saveOrderAjax()
+	{
+		// Get the input
+		$input = JFactory::getApplication()->input;
+		$pks   = $input->post->get('cid', array(), 'array');
+		$order = $input->post->get('order', array(), 'array');
+
+		// Sanitize the input
+		JArrayHelper::toInteger($pks);
+		JArrayHelper::toInteger($order);
+
+		// Get the model
+		$model = $this->getModel();
+
+		// Save the ordering
+		$return = $model->saveorder($pks, $order);
+
+		if ($return)
+		{
+			echo "1";
+		}
+
+		// Close the application
+		JFactory::getApplication()->close();
 	}
 }
