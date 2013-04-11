17fdac0e931e426ad848f71242ab31b9a465ba15
nha.redweb <nha@redweb.dk>
2013-04-11 14:06:49 +0700

    Update source code.

---
+++ components/com_redtwitter/admin/tables/oauth_info.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @version    1.0.0
+ * @package    Com_Redtwitter
+ * @author     Ronni K. G. Christiansen<email@redweb.dk> - http://www.redcomponent.com
+ * @copyright  Copyright (C) 2010 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ *             Developed by email@recomponent.com - redCOMPONENT.com
+ */
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Class RedtwitterTableOauth_Info
+ */
+class RedtwitterTableOauth_Info extends JTable
+{
+	/**
+	 * Constructor
+	 *
+	 * @param JDatabase A database connector object
+	 */
+	public function __construct(&$db)
+	{
+		parent::__construct('#__redtwitter_oauth_info', 'id', $db);
+	}
+}
