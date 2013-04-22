ecf5efa3025d3263afeaac59dd9aa62acb71338f
nha.redweb <nha@redweb.dk>
2013-04-22 11:34:48 +0700

    Commit helloworld part 14 and redtwitter

--- modules/mod_redtwitter/mod_redtwitter.php
+++ modules/mod_redtwitter/mod_redtwitter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version    Id: mod_redtwitter.php
+ * @version    1.0.0
  * @package    Com_Redtwitter
  * @author     Ronni K. G. Christiansen<email@redweb.dk> - http://www.redcomponent.com
  * @copyright  Copyright (C) 2010 redCOMPONENT.com. All rights reserved.
