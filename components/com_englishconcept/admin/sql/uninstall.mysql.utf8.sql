9c5587225c4c96859d66c5a928c89097663031a2
bngnha <bngnha@gmail.com>
2013-05-27 20:21:58 +0700

    Commit DB

--- components/com_englishconcept/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `#__lesson`;
-DROP TABLE IF EXISTS `#__lesson_script`;
-DROP TABLE IF EXISTS `#__lesson_key_structures`;
-DROP TABLE IF EXISTS `#__lesson_comprehension`;
-DROP TABLE IF EXISTS `#__lesson_special_difficulties`;
-DROP TABLE IF EXISTS `#__lesson_exercises`;
