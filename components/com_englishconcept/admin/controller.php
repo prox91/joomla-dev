57aa4054c48bb3fbeae6d121c34662b6711935b0
bngnha <bngnha@gmail.com>
2013-07-17 10:19:15 +0700

    add englishconcept component for testing...

---
+++ components/com_englishconcept/admin/controller.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ngoc Nha
+ * Date: 4/6/13
+ * Time: 10:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Import joomla controller library
+jimport('legacy.controller.legacy');
+
+class EnglishConceptController extends JControllerLegacy
+{
+	/**
+	 * Display task
+	 *
+	 * @return void
+	 */
+	public function display($cachable = false, $urlparams = array())
+	{
+		// Set default view if not set
+		$input = JFactory::getApplication()->input;
+		$input->set('view', $input->getCmd('view', 'EnglishConcepts'));
+
+		// Call parent behaviour
+		parent::display($cachable, $urlparams);
+	}
+}
