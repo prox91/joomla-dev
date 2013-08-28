0ddd8ea8b3eb7f986c6f5455bf3127bdccfc56a2
bngnha <bngnha@gmail.com>
2013-08-28 23:16:53 +0700

    Commit englishconcept

---
+++ components/com_englishconcept/admin/controllers/question.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 8/22/13
+ * Time: 11:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+defined('_JEXEC') or die('Restricted Access');
+
+class EnglishConceptControllerQuestion extends JControllerForm
+{
+	function __construct($config = array())
+	{
+		parent::__construct($config);
+	}
+}
