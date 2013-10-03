e5ecd74ea9fe87b997a076c03916fe609b169997
bngnha <bngnha@gmail.com>
2013-10-03 18:54:08 +0700

    Update restful library

---
+++ libraries/restful/code/controller/v1/json/testcontroller/get.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package     WebService.Controller
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * The class for Tag GET requests
+ *
+ * @package     WebService.Controller
+ * @subpackage  Controller
+ *
+ * @since       1.0
+ */
+class WebServiceControllerV1JsonTestcontrollerGet extends WebServiceControllerV1JsonBaseGet
+{
+	/**
+	 * Do the mapping with of tags with applications
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function doMap()
+	{
+		// Get application ID from input
+		$app_id = $this->input->get->getString('test_id');
+
+		// Check if application ID was passed
+		if (isset($app_id))
+		{
+			// Check if application exists in database
+			if ($this->itemExists($app_id, 'testcontroller'))
+			{
+				// Get content state
+				$modelState = $this->model->getState();
+
+				// Set content type that we need
+				$modelState->set('testcontroller.test_id', $app_id);
+			}
+			else
+			{
+				$this->app->errors->addError('204', array('test_id', $app_id));
+			}
+		}
+	}
+}
