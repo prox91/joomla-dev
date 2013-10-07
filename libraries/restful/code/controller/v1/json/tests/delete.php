ed4ffe7dd6408d765ce08bdabc45051d162bca81
bngnha <bngnha@gmail.com>
2013-10-07 22:51:24 +0700

    Update code for restful lib

---
+++ libraries/restful/code/controller/v1/json/tests/delete.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     WebService.Controller
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * The class for User DELETE requests
+ *
+ * @package     WebService.Controller
+ * @subpackage  Controller
+ *
+ * @since       1.0
+ */
+class WebServiceControllerV1JsonTestsDelete extends WebServiceControllerV1JsonBaseDelete
+{
+
+}
