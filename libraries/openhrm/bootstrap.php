ed4c9749ca0b97bcbe04ef76ee9aa8adefddd4e8
bngnha <bngnha@gmail.com>
2014-01-08 18:14:31 +0700

    commit source code hrm lib

--- libraries/openhrm/bootstrap.php
+++ libraries/openhrm/bootstrap.php
@@ -1,20 +1,18 @@
 <?php
 defined('JPATH_PLATFORM') or die;
-
 define('JPATH_OPENHRM', dirname(__FILE__));
 
 require JPATH_OPENHRM . '/functions.php';
 
-//// Use our own base field
-//if (!class_exists('JFormField', false))
-//{
-//	$baseField = JPATH_LIBRARIES . '/openhrm/joomla/form/field.php';
-//
-//	if (file_exists($baseField))
-//	{
-//		require_once $baseField;
-//	}
-//}
+if (!class_exists('JFormField', false))
+{
+	$baseField = JPATH_LIBRARIES . '/openhrm/joomla/form/field.php';
+
+	if (file_exists($baseField))
+	{
+		require_once $baseField;
+	}
+}
 
 // Register the classes for autoload.
 JLoader::registerPrefix('R', JPATH_OPENHRM);
@@ -22,15 +20,15 @@ JLoader::registerPrefix('R', JPATH_OPENHRM);
 // Setup the RLoader.
 RLoader::setup();
 
-// Make available the redCORE fields
+// Make available the openhrm fields
 JFormHelper::addFieldPath(JPATH_OPENHRM . '/form/fields');
 
-// Make available the redCORE form rules
+// Make available the openhrm form rules
 JFormHelper::addRulePath(JPATH_OPENHRM . '/form/rules');
 
 // HTML helpers
 JHtml::addIncludePath(JPATH_OPENHRM . '/html');
 
 // Load library language
-//$lang = JFactory::getLanguage();
-//$lang->load('lib_openhrm', JPATH_SITE);
+$lang = JFactory::getLanguage();
+$lang->load('lib_openhrm', JPATH_SITE);
