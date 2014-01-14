60134ff9a7aed8cfeba74ceda18d2d8b023d03d3
bngnha <bngnha@gmail.com>
2014-01-14 19:02:15 +0700

    commit source code hrm lib

---
+++ components/com_openhrm/admin/controllers/mediamanagers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 8/22/13
+ * Time: 11:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controlleradmin');
+
+class OpenHrmControllerMediaManagers extends JControllerAdmin
+{
+}
---
+++ components/com_openhrm/admin/views/mediamanagers/view.html.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ngoc Nha
+ * Date: 4/6/13
+ * Time: 10:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+// No direct access to this file
+defined('_JEXEC') or die;
+
+class OpenHrmViewMediaManagers extends OpenHrmViewAdmin
+{
+	protected $items;
+	protected $pagination;
+
+	/**
+	 * Display function
+	 */
+	public function display($tpl = null)
+	{
+		// Get data from the model
+        $this->state = $this->get('State');
+		$this->items = $this->get('Items');
+		$this->pagination = $this->get('Pagination');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+
+		// Display the template
+		parent::display($tpl);
+
+		// Set the documents
+		$this->setDocument();
+	}
+
+	/**
+	 * Get the view title.
+	 *
+	 * @return  string  The view title.
+	 */
+	public function getTitle()
+	{
+		return JText::_('COM_OPENHRM_COUNTRY_TITLE');
+	}
+
+    /**
+     * Get the view title.
+     *
+     * @return  string  The view title.
+     */
+    public function getTitleIcon()
+    {
+        return 'icon-globe';
+    }
+
+	public function setDocument()
+	{
+		$document = JFactory::getDocument();
+		$document->setTitle(JText::_("COM_OPENHRM_COUNTRY_TITLE"));
+	}
+}
